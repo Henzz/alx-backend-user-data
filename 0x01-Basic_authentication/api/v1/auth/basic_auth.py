623331c8b3a69a7273df3894a2f266314a40c2fd
Henzz <khenok53@gmail.com>
2024-08-10 06:33:10 +0300

    task 7 done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,23 @@ class BasicAuth(Auth):
     """
     Implements the Basic Authentication mechanism.
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Returns the Base64 part of the Authorization header for a
+        Basic Authentication.
+
+        Args:
+            authorization_header (str): The Authorization header to be checked.
+
+        Returns:
+            str: The Base64 part of the Authorization header, or None.
+        """
+        if authorization_header is None or not isinstance(authorization_header,
+                                                          str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header.split(" ")[1]
