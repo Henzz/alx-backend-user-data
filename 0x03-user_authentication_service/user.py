30dd7fa9db6290aac3fbbaa5ce0b37fa00dca70a
Henzz <khenok53@gmail.com>
2024-08-15 21:46:15 +0300

    fix

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,7 +2,7 @@
 """SQLAlchemy model for user
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
